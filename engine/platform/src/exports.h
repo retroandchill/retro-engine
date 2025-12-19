c586d0f3e8ecf7e7190e18eca581826b025d232b
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2025-12-19 17:55:42 -0500

    Updated the import definitions to ensure that we support other platforms correctly

--- engine/platform/src/exports.h
+++ engine/platform/src/exports.h
@@ -3,12 +3,16 @@
 //
 #pragma once
 
-#if defined(_WIN32) || defined(__CYGWIN__)
-    #define RETRO_API __declspec(dllexport)
-#else
-    #if __GNUC__ >= 4
-        #define RETRO_API __attribute__((visibility("default")))
+#ifndef RETRO_STATIC
+    #if defined(_WIN32) || defined(__CYGWIN__)
+        #define RETRO_API __declspec(dllexport)
     #else
-        #define RETRO_API
+        #if __GNUC__ >= 4
+            #define RETRO_API __attribute__((visibility("default")))
+        #else
+            #define RETRO_API
+        #endif
     #endif
+#else
+    #define RETRO_API
 #endif
