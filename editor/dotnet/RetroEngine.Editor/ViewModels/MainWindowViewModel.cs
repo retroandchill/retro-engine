4eb42053b5b02c7c48d74c3fd2348c974eae3adf
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2026-02-13 17:19:21 -0500

    Created an Avalonia template to test with

---
+++ editor/dotnet/RetroEngine.Editor/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace RetroEngine.Editor.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ editor/dotnet/RetroEngine.Editor/ViewModels/ViewModelBase.cs
@@ -0,0 +1,5 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace RetroEngine.Editor.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject { }
