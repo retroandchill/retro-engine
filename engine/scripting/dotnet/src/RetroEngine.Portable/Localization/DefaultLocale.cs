47739309987066beb526150e0dadc1aca71634ad
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2026-02-16 16:57:55 -0500

    Added region info to DefaultLocale

--- engine/scripting/dotnet/src/RetroEngine.Portable/Localization/DefaultLocale.cs
+++ engine/scripting/dotnet/src/RetroEngine.Portable/Localization/DefaultLocale.cs
@@ -11,6 +11,7 @@ namespace RetroEngine.Portable.Localization;
 public sealed class DefaultLocale : ILocale
 {
     private readonly CultureInfo _cultureInfo;
+    private readonly RegionInfo _regionInfo;
     public string DisplayName => _cultureInfo.DisplayName;
     public string EnglishName => _cultureInfo.EnglishName;
     public int KeyboardLayoutId => _cultureInfo.KeyboardLayoutId;
@@ -20,7 +21,7 @@ public sealed class DefaultLocale : ILocale
     public string ThreeLetterISOLanguageName => _cultureInfo.ThreeLetterISOLanguageName;
     public string TwoLetterISOLanguageName => _cultureInfo.TwoLetterISOLanguageName;
     public string NativeLanguage { get; }
-    public string NativeRegion { get; }
+    public string NativeRegion => _regionInfo.NativeName;
     public string Script { get; }
     public string Variant { get; }
     public bool IsRightToLeft => _cultureInfo.TextInfo.IsRightToLeft;
@@ -30,6 +31,7 @@ public sealed class DefaultLocale : ILocale
     public DefaultLocale(CultureInfo cultureInfo)
     {
         _cultureInfo = cultureInfo;
+        _regionInfo = new RegionInfo(cultureInfo.LCID);
     }
 
     public DecimalNumberFormattingRules GetCurrencyFormattingRules(string currencyCode)
