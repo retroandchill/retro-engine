d479f6d7c2404272f0af6ceee245e9eb30ed1066
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2026-02-19 21:51:59 -0500

    Experimented with calling into the ICU library directly from .NET

--- engine/scripting/dotnet/test/RetroEngine.Portable.Test/Localization/FormatTextTest.cs
+++ engine/scripting/dotnet/test/RetroEngine.Portable.Test/Localization/FormatTextTest.cs
@@ -3,12 +3,24 @@
 // // @copyright Copyright (c) 2026 Retro & Chill. All rights reserved.
 // // Licensed under the MIT License. See LICENSE file in the project root for full license information.
 
+using System.Text;
+using RetroEngine.Portable.Localization;
 using RetroEngine.Portable.Localization.Formatting;
 
 namespace RetroEngine.Portable.Test.Localization;
 
 public class FormatTextTest
 {
+    [Test]
+    public unsafe void TestIcuInterop()
+    {
+        var enBytes = Encoding.UTF8.GetBytes("en_US");
+        Span<char> targetBuffer = stackalloc char[32];
+        var resultString = IcuInterop.GetDisplayName(enBytes, enBytes, targetBuffer);
+        var targetString = new string(resultString.ToArray());
+        Assert.That(targetString, Is.EqualTo("en_US"));
+    }
+
     [Test]
     public void CanDenotePluralForms()
     {
