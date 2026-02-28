e537eb0b63bbb03495fea22bde02b421bd60bab7
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2026-02-28 13:45:39 -0500

    Fixed certain symbols not having correct linkage

--- engine/src/runtime/cpp/interop/private/localization/collator.cpp
+++ engine/src/runtime/cpp/interop/private/localization/collator.cpp
@@ -9,34 +9,37 @@
 import std;
 import retro.core.localization.icu;
 
-RETRO_API icu::Collator *retro_create_collator(const icu::Locale *locale)
+extern "C"
 {
-    UErrorCode status;
-    return icu::Collator::createInstance(*locale, status);
-}
+    RETRO_API icu::Collator *retro_create_collator(const icu::Locale *locale)
+    {
+        UErrorCode status;
+        return icu::Collator::createInstance(*locale, status);
+    }
 
-RETRO_API void retro_destroy_collator(const icu::Collator *collator)
-{
-    delete collator;
-}
+    RETRO_API void retro_destroy_collator(const icu::Collator *collator)
+    {
+        delete collator;
+    }
 
-RETRO_API icu::Collator *retro_collator_clone(const icu::Collator *collator)
-{
-    return collator->clone();
-}
+    RETRO_API icu::Collator *retro_collator_clone(const icu::Collator *collator)
+    {
+        return collator->clone();
+    }
 
-RETRO_API void retro_collator_set_strength(icu::Collator *collator, std::int32_t strength)
-{
-    collator->setStrength(static_cast<icu::Collator::ECollationStrength>(strength));
-}
+    RETRO_API void retro_collator_set_strength(icu::Collator *collator, std::int32_t strength)
+    {
+        collator->setStrength(static_cast<icu::Collator::ECollationStrength>(strength));
+    }
 
-RETRO_API std::int32_t retro_collator_compare(const icu::Collator *collator,
-                                              const char16_t *lhs,
-                                              const std::int32_t lhs_length,
-                                              const char16_t *rhs,
-                                              const std::int32_t rhs_length)
-{
-    const icu::UnicodeString lhs_str{lhs, lhs_length};
-    const icu::UnicodeString rhs_str{rhs, rhs_length};
-    return collator->compare(lhs_str, rhs_str);
+    RETRO_API std::int32_t retro_collator_compare(const icu::Collator *collator,
+                                                  const char16_t *lhs,
+                                                  const std::int32_t lhs_length,
+                                                  const char16_t *rhs,
+                                                  const std::int32_t rhs_length)
+    {
+        const icu::UnicodeString lhs_str{lhs, lhs_length};
+        const icu::UnicodeString rhs_str{rhs, rhs_length};
+        return collator->compare(lhs_str, rhs_str);
+    }
 }
