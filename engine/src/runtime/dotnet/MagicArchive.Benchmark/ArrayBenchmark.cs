89d10f07046e27cdc9550e296691ef9e68307dd6
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2026-04-13 22:08:19 -0400

    Created a simple benchmark project as a baseline

---
+++ engine/src/runtime/dotnet/MagicArchive.Benchmark/ArrayBenchmark.cs
@@ -0,0 +1,29 @@
+﻿// // @file ListBenchmark.cs
+// //
+// // @copyright Copyright (c) 2026 Retro & Chill. All rights reserved.
+// // Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+using System.Text.Json;
+using BenchmarkDotNet.Attributes;
+
+namespace MagicArchive.Benchmark;
+
+[MemoryDiagnoser]
+public class ArrayBenchmark
+{
+    private readonly int[] _numbers = Enumerable.Range(1, 1000).ToArray();
+
+    [Benchmark]
+    public int SerializeToJson()
+    {
+        var serialized = JsonSerializer.Serialize(_numbers);
+        return serialized.Length;
+    }
+
+    [Benchmark]
+    public int SerializeToBinary()
+    {
+        var serialized = ArchiveSerializer.Serialize(_numbers);
+        return serialized.Length;
+    }
+}
---
+++ engine/src/runtime/dotnet/MagicArchive.Benchmark/Program.cs
@@ -0,0 +1,5 @@
+﻿using BenchmarkDotNet.Running;
+using MagicArchive.Benchmark;
+
+var switcher = new BenchmarkSwitcher([typeof(ArrayBenchmark), typeof(ListBenchmark)]);
+switcher.Run(args);
