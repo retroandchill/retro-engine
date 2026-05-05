720f4ea0c5dc3eb4747442171b6bf668d49eb519
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2026-05-05 17:27:36 -0400

    Created a basic outline of how we might define a type to represent a script type

---
+++ engine/src/runtime/dotnet/main/RetroEngine.Scripting/Model/IScriptType.cs
@@ -0,0 +1,19 @@
+﻿// // @file IScriptType.cs
+// //
+// // @copyright Copyright (c) 2026 Retro & Chill. All rights reserved.
+// // Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+namespace RetroEngine.Scripting.Model;
+
+public interface IScriptType
+{
+    Type? ClrType { get; }
+
+    string Name { get; }
+
+    string? Namespace { get; }
+
+    string? FullName { get; }
+
+    string? AssemblyQualifiedName { get; }
+}
